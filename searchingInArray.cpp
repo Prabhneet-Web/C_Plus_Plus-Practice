f6ee269e71b5d5d582cd072484e9220f2e030daf
Prabhneet Narula <imailcode@gmail.com>
2022-05-11 02:07:34 +0530

    searchingInArray added.

---
+++ searchingInArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    int m, n;
+    std::cin >> m >> n;
+    int arr[m][n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            std::cin >> arr[i][j];
+        }
+    }
+    std::cout << "Enter the key: ";
+
+    int key;
+    std::cin >> key;
+    bool flag = false;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (key == arr[i][j])
+                std::cout << "found!\n";
+            flag = true;
+        }
+    }
+    if (flag == false)
+        std::cout << "not-found\n";
+}
